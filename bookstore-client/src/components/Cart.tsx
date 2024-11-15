5a2c20c9ee38d8f718b3776ae8c3d83744000a0b
plamenovv11 <iv._plamenov@abv.bg>
2024-11-14 20:29:01 -0800

    add login/logout auth logic

--- bookstore-client/src/components/Cart.tsx
+++ bookstore-client/src/components/Cart.tsx
@@ -7,16 +7,15 @@ const Cart: React.FC = () => {
             <Box
                 sx={{
                     display: 'flex',
-                    justifyContent: 'space-between', // Ensures the items are spaced out
-                    alignItems: 'center', // Vertically centers the items
-                    padding: 2, // Adds padding around the box
-                    border: '1px solid #ccc', // Optional border for the frame
-                    borderRadius: '8px', // Optional rounded corners for the frame
-                    boxShadow: 3, // Optional shadow for visual enhancement
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    padding: 2,
+                    border: '1px solid #ccc',
+                    borderRadius: '8px',
+                    boxShadow: 3,
                     marginBottom: 4
                 }}
             >
-
             </Box>
         </React.Fragment>
     );
